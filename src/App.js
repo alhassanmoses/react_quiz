13c2d4243f10d0f97a01930b9ad1a9b3e0b0515e
alhassanmoses <alhassanmoses.amw@gmail.com>
2020-08-28 19:10:02 +0000

    finished

--- src/App.js
+++ src/App.js
@@ -1,41 +1,95 @@
-import React, { Component } from 'react';
-
-
-// state data for 3 counters
-const data = [
-  { id: 1, value: 0 },
-  { id: 2, value: 0 },
-  { id: 3, value: 0 },
-];
+import React, { Component } from "react";
 
 // Counter Component
 class Counter extends Component {
   render() {
-    const { value } = this.props;
+    let { index, value, Increment, Decrement } = this.props;
+
     return (
       <div className="counter">
         <b>{value}</b>
         <div className="counter-controls">
-          <button className="button is-danger is-small">-</button>
-          <button className="button is-success is-small">+</button>
+          <button
+            className="button is-danger is-small"
+            onClick={() => Decrement(index, value)}
+          >
+            -
+          </button>
+          <button
+            className="button is-success is-small"
+            onClick={() => Increment(index, value)}
+          >
+            +
+          </button>
         </div>
       </div>
     );
   }
 }
+class Total extends Component {
+  total = 0;
+
+  render() {
+    let array = this.props.data;
+
+    array.data.map((sum) => {
+      this.total += sum.value;
+    });
+    return <div>{<h1>{this.total}</h1>}</div>;
+  }
+}
 
 class App extends Component {
+  // state data for 3 counters
+  data = [
+    { id: 1, value: 0 },
+    { id: 2, value: 0 },
+    { id: 3, value: 0 },
+    { id: 4, value: 0 },
+  ];
+
   constructor(props, context) {
     super(props, context);
+
+    this.state = { data: [...this.data] };
   }
+
+  onIncrement = (index, value) => {
+    //
+    console.log(`I called ${index}`);
+    //
+    let temp = this.state;
+    temp.data[index].value = ++value;
+    console.log(`Value is ${value}`);
+    this.setState(temp);
+  };
+  onDecrement = (index, value) => {
+    //
+    console.log(`D called ${index}`);
+    //
+    let temp = this.state;
+    console.log(temp.data[index].value);
+
+    temp.data[index].value = --value;
+    this.setState(temp);
+  };
 
   render() {
     return (
-      <div>
-        {data.map((counter) => (
-          <Counter key={counter.id} value={counter.value} />
-        ))}
-      </div>
+      <>
+        <div>
+          {this.state.data.map((counter, key) => (
+            <Counter
+              key={key}
+              index={counter.id - 1}
+              value={counter.value}
+              Increment={this.onIncrement}
+              Decrement={this.onDecrement}
+            />
+          ))}
+        </div>
+        <Total data={this.state} />
+      </>
     );
   }
 }
